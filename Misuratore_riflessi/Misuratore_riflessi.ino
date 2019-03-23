774c4c617b8cbb91cc50837a23b3dfbc43fa6511
Alessandro <alexvulcu21@gmail.com>
2019-03-23 15:41:35 +0100

    23/03/2019 - aggiunta del tempo in cui l'utente deve cliccare il bottone (250 ms),che se viene superato fa ricominciare il programma, riduzione del loop a soli due richiami di funzioni

--- Misuratore_riflessi/Misuratore_riflessi.ino
+++ Misuratore_riflessi/Misuratore_riflessi.ino
@@ -3,10 +3,14 @@ int led = 13;
 bool stato_pulsante;
 int button = 8;
 int buzzer = 10;
-int tempo1;
-int tempo2;
+double tempo1;
+double tempo2;
+int t1,t2;
 int cas1;
 int cas2;
+int i = 0;
+double time1 = 0;
+double time2 = 0;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -16,7 +20,6 @@ void setup() {
   pinMode(led, OUTPUT);
   pinMode(button, INPUT);
   pinMode(buzzer, OUTPUT);
-  
 }
 
 void inizia_lcd(){
@@ -24,27 +27,44 @@ void inizia_lcd(){
   lcd.print("CLICK THE BUTTON");
   lcd.setCursor(0,1);
   lcd.print("TO START :)");
+  loop();
 }
 
-void accensione_led(){
-  randomSeed(analogRead(0));
-  cas1 = random(1000, 7000);
-  delay(cas1);
-  stato_pulsante = 0;
-  digitalWrite(led, HIGH);
-  tempo1 = millis(); //salva dentro una variabile il tempo trascorso dall'accensione dell'Arduino
-  
-  while(stato_pulsante == 0){
+void accensione_led(){  
+    randomSeed(analogRead(0));
+    cas1 = random(1000, 7000);
+    delay(cas1);
+    digitalWrite(led, HIGH);
+    stato_pulsante = 0;
+    tempo1 = millis(); //salva dentro una variabile il tempo trascorso dall'accensione dell'Arduino
     
+  while(stato_pulsante == LOW){
     stato_pulsante = digitalRead(button);
     if(stato_pulsante == HIGH){
       tempo2 = millis();//salva dentro una seconda variabile il tempo trascorso dall'accensione dell'Arduino
       digitalWrite(led, LOW);
-      
     }
-  }
-  lcd.setCursor(0,0);
-  lcd.print(tempo2 - tempo1);//la differenza tra questi due tempi dà il tempo impegato per cliccare il bottone all'accensione del led
+    
+    }
+    
+    time1 = tempo2 - tempo1;
+    
+    if(time1 > 250){
+    lcd.setCursor(0,0);
+    lcd.print("Troppo lento");
+    delay(3000);
+    lcd.clear();
+    stato_pulsante = 0;
+    setup();
+    }
+    else{
+      lcd.setCursor(0,0);
+      lcd.print(time1/1000);
+      lcd.setCursor(4,0);
+      lcd.print(" s");//la differenza tra questi due tempi dà il tempo impegato per cliccare il bottone all'accensione del led
+      suono_buzzer();
+    }
+    
 }
 
 void suono_buzzer(){
@@ -63,22 +83,54 @@ void suono_buzzer(){
       noTone(buzzer);
     }
   }
-  lcd.setCursor(0,1);
-  lcd.print(tempo2 - tempo1);//la differenza tra questi due tempi dà il tempo impegato per cliccare il bottone al suono del buzzer
-  delay(5000);
-
+   time2 = tempo2 - tempo1;
+   
+   if(time2 > 250){
+    lcd.setCursor(0,1);
+    lcd.print("Troppo lento");
+    delay(3000);
+    lcd.clear();
+    stato_pulsante = 0;
+    setup();
+    
+   }
+   else{
+   lcd.setCursor(0,1);
+   lcd.print(time2/1000);
+   lcd.setCursor(4,1);
+   lcd.print(" s");//la differenza tra questi due tempi dà il tempo impegato per cliccare il bottone al suono del buzzer
+   delay(1000);
+   }
+   
+   
+   stato_pulsante = 0;
+   
+   while(stato_pulsante == 0){
+    
+    stato_pulsante = digitalRead(button);
+    if(stato_pulsante == 1){
+      stato_pulsante = 0;
+      delay(100);
+      setup();
+    }
+   }
+   
 }
-
+void start(){
+  stato_pulsante = 0;
+  while(stato_pulsante == 0){
+    stato_pulsante = digitalRead(button);
+    if(stato_pulsante == 1){
+      lcd.clear();
+      accensione_led();
+    }
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  stato_pulsante = digitalRead(button);
-  if(stato_pulsante == 1){
-    lcd.clear();
-    delay(5000);
-  accensione_led();
-  suono_buzzer();
+  start();
   setup();
     
-  }
+  
 }
